d90a96ec5f3defb29fd7d61f8437dc81ea426aa8
momnpa333 <momnpa333@gmail.com>
2024-07-24 16:03:40 +0900

    feat: Repository에 Transactional 추가

--- src/main/java/gift/repository/token/TokenInMemoryRepository.java
+++ src/main/java/gift/repository/token/TokenInMemoryRepository.java
@@ -4,6 +4,7 @@ import gift.model.product.Product;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class TokenInMemoryRepository implements TokenRepository {
@@ -12,21 +13,25 @@ public class TokenInMemoryRepository implements TokenRepository {
     private final Map<Long, String> RefreshTokenStorage = new ConcurrentHashMap<>();
 
     @Override
+    @Transactional
     public void saveAccessToken(Long userId, String accessToken) {
         AccessTokenStorage.put(userId, accessToken);
     }
 
     @Override
+    @Transactional
     public void saveRefreshToken(Long userId, String refreshToken) {
         RefreshTokenStorage.put(userId, refreshToken);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public String getAccessToken(Long userId) {
         return AccessTokenStorage.get(userId);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public String getRefreshToken(Long userId) {
         return RefreshTokenStorage.get(userId);
     }
