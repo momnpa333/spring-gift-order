7ee33577e629d4f5c7cf779709622d519f313029
momnpa333 <momnpa333@gmail.com>
2024-07-25 00:37:02 +0900

    feat: OAuthService->MemberOAuthService 이름 변경

--- src/main/java/gift/application/member/service/MemberOAuthService.java
+++ src/main/java/gift/application/member/service/MemberOAuthService.java
@@ -3,29 +3,25 @@ package gift.application.member.service;
 import com.fasterxml.jackson.databind.JsonNode;
 import gift.application.member.dto.OAuthCommand;
 import gift.application.member.service.apicaller.KakaoMemberApiCaller;
+import gift.application.token.TokenManager;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OAuthService {
+public class MemberOAuthService {
 
     private final KakaoMemberApiCaller kakaoMemberApiCaller;
+    private final TokenManager tokenManager;
 
-    public OAuthService(KakaoMemberApiCaller kaKaoMemberApiCaller) {
+    public MemberOAuthService(KakaoMemberApiCaller kaKaoMemberApiCaller,
+        TokenManager tokenManager) {
         this.kakaoMemberApiCaller = kaKaoMemberApiCaller;
-    }
-
-    /**
-     * 인가 코드를 사용해서 토큰 가져오기
-     */
-    private String getAccessToken(OAuthCommand.Login command) {
-        return kakaoMemberApiCaller.getAccessToken(command.authorizationCode());
+        this.tokenManager = tokenManager;
     }
 
     /**
      * 토큰을 사용해서 사용자 정보를 가져오는 로직
      */
-    public OAuthCommand.MemberInfo getMemberInfo(OAuthCommand.Login command) {
-        String accessToken = getAccessToken(command);
+    public OAuthCommand.MemberInfo getMemberInfo(String accessToken) {
         JsonNode userInfo = kakaoMemberApiCaller.getMemberInfo(accessToken);
         return OAuthCommand.MemberInfo.from(userInfo);
     }
