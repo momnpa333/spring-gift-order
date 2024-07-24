da596ea9804d381e5ead4f4f259bc1c0d49d2f54
momnpa333 <momnpa333@gmail.com>
2024-07-25 00:34:43 +0900

    feat: KakaoTokenApiCaller 에서 인가코드로 토큰 가져오는 로직 추가

--- src/main/java/gift/application/token/apicaller/KakaoTokenApiCaller.java
+++ src/main/java/gift/application/token/apicaller/KakaoTokenApiCaller.java
@@ -1,23 +1,30 @@
 package gift.application.token.apicaller;
 
+import static io.jsonwebtoken.Header.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
+
 import com.fasterxml.jackson.databind.JsonNode;
+import gift.application.token.dto.TokenSet;
+import gift.global.validate.TimeOutException;
 import java.net.URI;
 import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class KakaoTokenApiCaller {
 
-    private static final String KAKAO_USER_INFO_URL = "https://kapi.kakao.com/v2/user/me";
     private static final String KAKAO_TOKEN_URL = "https://kauth.kakao.com/oauth/token";
-    private static final String AUTHORIZATION_GRANT_TYPE = "authorization_code";
+    private static final String GRANT_TYPE = "authorization_code";
     private static final String REFRESH_GRANT_TYPE = "refresh_token";
     private static final Duration TIMEOUT = Duration.ofSeconds(2);
 
@@ -36,9 +43,31 @@ public class KakaoTokenApiCaller {
     }
 
     /**
+     * 인가 코드를 사용해서 토큰을 가져옴
+     */
+    public TokenSet getTokens(String authorizationCode) {
+        var headers = new HttpHeaders();
+        headers.add(CONTENT_TYPE, APPLICATION_FORM_URLENCODED_VALUE);
+        var body = createGetAccessTokenBody(authorizationCode);
+        System.out.println(body);
+        var request = new RequestEntity<>(body, headers, HttpMethod.POST,
+            URI.create(KAKAO_TOKEN_URL));
+        try {
+            ResponseEntity<JsonNode> response = restTemplate.exchange(request, JsonNode.class);
+            String accessToken = response.getBody().get("access_token").asText();
+            String refreshToken = response.getBody().get("refresh_token").asText();
+            return new TokenSet(accessToken, refreshToken);
+        } catch (ResourceAccessException e) {
+            throw new TimeOutException("네트워크 연결이 불안정 합니다.", e);
+        } catch (HttpClientErrorException e) {
+            throw new IllegalArgumentException("카카오 인가 코드가 유효하지 않습니다.", e);
+        }
+    }
+
+    /**
      * refresh token을 사용해서 access token을 갱신
      */
-    public String refreshAccessToken(String refreshToken) {
+    public TokenSet refreshAccessToken(String refreshToken) {
         var headers = new LinkedMultiValueMap<String, String>();
         headers.add("Content-Type", "application/x-www-form-urlencoded");
         var body = createUpdateAccessTokenBody(refreshToken);
@@ -46,7 +75,9 @@ public class KakaoTokenApiCaller {
             URI.create(KAKAO_TOKEN_URL));
         try {
             ResponseEntity<JsonNode> response = restTemplate.exchange(request, JsonNode.class);
-            return response.getBody().get("access_token").asText();
+            String newAccessToken = response.getBody().get("access_token").asText();
+            String newRefreshToken = response.getBody().get("refresh_token").asText();
+            return new TokenSet(newAccessToken, newRefreshToken);
         } catch (Exception e) {
             throw new RuntimeException("토큰 갱신에 실패했습니다.", e);
         }
@@ -55,10 +86,20 @@ public class KakaoTokenApiCaller {
     private LinkedMultiValueMap<String, String> createUpdateAccessTokenBody(
         String refreshToken) {
         var body = new LinkedMultiValueMap<String, String>();
-
-        body.add("grant_type", AUTHORIZATION_GRANT_TYPE);
+        body.add("grant_type", REFRESH_GRANT_TYPE);
         body.add("client_id", CLIENT_ID);
         body.add("refresh_token", refreshToken);
+        return body;
+    }
+
+    private LinkedMultiValueMap<String, String> createGetAccessTokenBody(
+        String authorizationCode) {
+        var body = new LinkedMultiValueMap<String, String>();
+
+        body.add("grant_type", GRANT_TYPE);
+        body.add("client_id", CLIENT_ID);
+        body.add("redirect_uri", REDIRECT_URI);
+        body.add("code", authorizationCode);
         return body;
     }
 
