d055b11b568cf0587869605a4813fabfa25b9f6c
momnpa333 <momnpa333@gmail.com>
2024-07-25 00:34:03 +0900

    feat: KakaoMemberApiCaller 에서 인가코드로 토큰 가져오는 로직 제거

--- src/main/java/gift/application/member/service/apicaller/KakaoMemberApiCaller.java
+++ src/main/java/gift/application/member/service/apicaller/KakaoMemberApiCaller.java
@@ -43,26 +43,6 @@ public class KakaoMemberApiCaller {
     }
 
     /**
-     * 인가 코드를 사용해서 토큰을 가져옴
-     */
-    public String getAccessToken(String authorizationCode) {
-        var headers = new HttpHeaders();
-        headers.add(CONTENT_TYPE, APPLICATION_FORM_URLENCODED_VALUE);
-        var body = createGetAccessTokenBody(authorizationCode);
-        System.out.println(body);
-        var request = new RequestEntity<>(body, headers, HttpMethod.POST,
-            URI.create(KAKAO_TOKEN_URL));
-        try {
-            ResponseEntity<JsonNode> response = restTemplate.exchange(request, JsonNode.class);
-            return response.getBody().get("access_token").asText();
-        } catch (ResourceAccessException e) {
-            throw new TimeOutException("네트워크 연결이 불안정 합니다.", e);
-        } catch (HttpClientErrorException e) {
-            throw new IllegalArgumentException("카카오 인가 코드가 유효하지 않습니다.", e);
-        }
-    }
-
-    /**
      * 토큰을 사용해서 사용자 정보를 가져옴
      */
     public JsonNode getMemberInfo(String accessToken) {
@@ -77,16 +57,5 @@ public class KakaoMemberApiCaller {
         } catch (ResourceAccessException e) {
             throw new TimeOutException("네트워크 연결이 불안정 합니다.", e);
         }
-    }
-
-    private LinkedMultiValueMap<String, String> createGetAccessTokenBody(
-        String authorizationCode) {
-        var body = new LinkedMultiValueMap<String, String>();
-
-        body.add("grant_type", GRANT_TYPE);
-        body.add("client_id", CLIENT_ID);
-        body.add("redirect_uri", REDIRECT_URI);
-        body.add("code", authorizationCode);
-        return body;
     }
 }
