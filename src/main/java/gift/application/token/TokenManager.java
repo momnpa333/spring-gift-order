871d8af12a4897d9db7a7d50ad80750c95b0303a
momnpa333 <momnpa333@gmail.com>
2024-07-25 00:33:18 +0900

    feat: TokenManager 제작

--- src/main/java/gift/application/token/TokenManager.java
+++ src/main/java/gift/application/token/TokenManager.java
@@ -1,18 +1,32 @@
 package gift.application.token;
 
 import gift.application.token.apicaller.KakaoTokenApiCaller;
+import gift.application.token.dto.TokenSet;
 import gift.repository.token.TokenRepository;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TokenProvider {
+public class TokenManager {
 
     private final KakaoTokenApiCaller kakaoTokenApiCaller;
     private final TokenRepository tokenRepository;
 
-    public TokenProvider(KakaoTokenApiCaller kakaoTokenApiCaller, TokenRepository tokenRepository) {
+    public TokenManager(KakaoTokenApiCaller kakaoTokenApiCaller, TokenRepository tokenRepository) {
         this.tokenRepository = tokenRepository;
         this.kakaoTokenApiCaller = kakaoTokenApiCaller;
+    }
+
+    /**
+     * 인가 코드를 사용해서 토큰 가져오기
+     */
+    public TokenSet getTokens(String authorizationCode) {
+        return kakaoTokenApiCaller.getTokens(authorizationCode);
+    }
+
+    public void saveTokens(Long userId, TokenSet tokenSet) {
+        tokenRepository.saveAccessToken(userId, tokenSet.accessToken());
+        tokenRepository.saveRefreshToken(userId, tokenSet.refreshToken());
     }
 
     public void saveAccessToken(Long userId, String accessToken) {
@@ -33,8 +47,9 @@ public class TokenProvider {
 
     public String updateAccessToken(Long userId) {
         String refreshToken = getRefreshToken(userId);
-        String newAccessToken = kakaoTokenApiCaller.refreshAccessToken(refreshToken);
-        saveAccessToken(userId, newAccessToken);
-        return newAccessToken;
+        TokenSet tokenSet = kakaoTokenApiCaller.refreshAccessToken(refreshToken);
+        tokenRepository.saveAccessToken(userId, tokenSet.accessToken());
+        tokenRepository.saveRefreshToken(userId, tokenSet.refreshToken());
+        return tokenSet.accessToken();
     }
 }
