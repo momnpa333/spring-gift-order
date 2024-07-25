a5d3c5f5d7433307249acb64a2e3736770c4efc4
momnpa333 <momnpa333@gmail.com>
2024-07-25 23:21:17 +0900

    feat: purchaseOption 반환값 변경

--- src/main/java/gift/application/product/service/OptionService.java
+++ src/main/java/gift/application/product/service/OptionService.java
@@ -78,10 +78,11 @@ public class OptionService {
         backoff = @Backoff(delay = 200)
     )
     @Transactional
-    public void purchaseOption(OptionCommand.Purchase command) {
+    public OptionModel.Info purchaseOption(OptionCommand.Purchase command) {
         Option option = optionRepository.findById(command.optionId())
             .orElseThrow(() -> new NotFoundException("Option not found"));
         option.purchase(command.quantity());
+        return OptionModel.Info.from(option);
     }
 
 
