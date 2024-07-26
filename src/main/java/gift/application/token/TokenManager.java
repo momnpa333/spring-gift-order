f7a8ce52b4d32f73a357510329616bf7732a2b8a
momnpa333 <momnpa333@gmail.com>
2024-07-26 14:51:51 +0900

    refact: tokenManager 리팩토링

--- src/main/java/gift/application/token/TokenManager.java
+++ src/main/java/gift/application/token/TokenManager.java
@@ -2,8 +2,8 @@ package gift.application.token;
 
 import gift.application.token.apicaller.KakaoTokenApiCaller;
 import gift.application.token.dto.TokenSet;
+import gift.model.token.KakaoToken;
 import gift.repository.token.TokenRepository;
-import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,36 +20,21 @@ public class TokenManager {
     /**
      * 인가 코드를 사용해서 토큰 가져오기
      */
-    public TokenSet getTokens(String authorizationCode) {
-        return kakaoTokenApiCaller.getTokens(authorizationCode);
+    public KakaoToken getTokenAndSaveByAuthorizationCode(Long memberId, String authorizationCode) {
+        KakaoToken token = kakaoTokenApiCaller.getToken(authorizationCode);
+        tokenRepository.saveToken(memberId, token);
+        return token;
     }
 
-    public void saveTokens(Long userId, TokenSet tokenSet) {
-        tokenRepository.saveAccessToken(userId, tokenSet.accessToken());
-        tokenRepository.saveRefreshToken(userId, tokenSet.refreshToken());
-    }
+    public KakaoToken getToken(Long userId) {
+        KakaoToken token = tokenRepository.getToken(userId);
 
-    public void saveAccessToken(Long userId, String accessToken) {
-        tokenRepository.saveAccessToken(userId, accessToken);
-    }
+        if (token.isValid()) {
+            return token;
+        }
 
-    public void saveRefreshToken(Long userId, String refreshToken) {
-        tokenRepository.saveRefreshToken(userId, refreshToken);
-    }
-
-    public String getAccessToken(Long userId) {
-        return tokenRepository.getAccessToken(userId);
-    }
-
-    public String getRefreshToken(Long userId) {
-        return tokenRepository.getRefreshToken(userId);
-    }
-
-    public String updateAccessToken(Long userId) {
-        String refreshToken = getRefreshToken(userId);
-        TokenSet tokenSet = kakaoTokenApiCaller.refreshAccessToken(refreshToken);
-        tokenRepository.saveAccessToken(userId, tokenSet.accessToken());
-        tokenRepository.saveRefreshToken(userId, tokenSet.refreshToken());
-        return tokenSet.accessToken();
+        KakaoToken newToken = kakaoTokenApiCaller.refreshAccessToken(token.getRefreshToken());
+        tokenRepository.saveToken(userId, newToken);
+        return newToken;
     }
 }
