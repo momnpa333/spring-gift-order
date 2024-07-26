86b1b240969c3114cfa80e6344aa1953cd83f322
momnpa333 <momnpa333@gmail.com>
2024-07-26 17:24:39 +0900

    refact: JsonNode->dto 로 변환

--- src/main/java/gift/application/member/service/apicaller/MemberKakaoApiCaller.java
+++ src/main/java/gift/application/member/service/apicaller/MemberKakaoApiCaller.java
@@ -2,7 +2,7 @@ package gift.application.member.service.apicaller;
 
 import static io.jsonwebtoken.Header.CONTENT_TYPE;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import gift.application.member.dto.MemberKakaoModel;
 import gift.global.validate.TimeOutException;
 import java.net.URI;
 import java.time.Duration;
@@ -11,7 +11,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
@@ -34,15 +33,16 @@ public class MemberKakaoApiCaller {
     /**
      * 토큰을 사용해서 사용자 정보를 가져옴
      */
-    public JsonNode getMemberInfo(String accessToken) {
+    public MemberKakaoModel.MemberInfo getMemberInfo(String accessToken) {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + accessToken);
         headers.add(CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
         var request = new RequestEntity<>(headers, HttpMethod.GET, URI.create(KAKAO_USER_INFO_URL));
 
         try {
-            ResponseEntity<JsonNode> responseNode = restTemplate.exchange(request, JsonNode.class);
-            return responseNode.getBody();
+            MemberKakaoModel.MemberInfo memberInfo = restTemplate.exchange(request,
+                MemberKakaoModel.MemberInfo.class).getBody();
+            return memberInfo;
         } catch (ResourceAccessException e) {
             throw new TimeOutException("네트워크 연결이 불안정 합니다.", e);
         }
