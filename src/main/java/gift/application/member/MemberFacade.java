76c8fe4a7ab1bb903965e9742b6dafdfa60a94a2
momnpa333 <momnpa333@gmail.com>
2024-07-27 19:46:10 +0900

    refact: socialLogin 로직 변경

--- src/main/java/gift/application/member/MemberFacade.java
+++ src/main/java/gift/application/member/MemberFacade.java
@@ -1,12 +1,12 @@
 package gift.application.member;
 
 import gift.application.member.dto.MemberCommand;
+import gift.application.member.dto.MemberModel;
 import gift.application.member.dto.OAuthCommand;
 import gift.application.member.service.MemberService;
 import gift.application.member.service.MemberKakaoService;
 import gift.application.token.TokenManager;
 import gift.model.token.KakaoToken;
-import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,9 +31,9 @@ public class MemberFacade {
         OAuthCommand.MemberInfo memberInfo = memberKakaoService.getMemberInfo(
             token.getAccessToken());
         MemberCommand.Create create = memberInfo.toCreateCommand();
-        Pair<Long, String> memberIdAndJwt = memberService.socialLogin(create);
-        tokenManager.saveToken(memberIdAndJwt.getFirst(), token);
-        return memberIdAndJwt.getSecond();
+        MemberModel.IdAndJwt memberIdAndJwt = memberService.socialLogin(create);
+        tokenManager.saveToken(memberIdAndJwt.memberId(), token);
+        return memberIdAndJwt.jwt();
     }
 
 }
